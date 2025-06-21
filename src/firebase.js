885839616a6ded8e9f6a9636fbf07198f2c5bf4f
alanberstein1 <alanberstein1@gmail.com>
2025-06-21 18:38:21 -0400

    Updated src/firebase.js for login to firebase SDK/App

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Replace with your real config:
 const firebaseConfig = {
-  apiKey: "pAsuFSSBSeWMNflIqZbNEBd4n-d7hdmOXnfWMwL4fo4",
+  apiKey: "AIzaSyAVFMND9n0LPPgKir7JK_5Sr4ANl-oiLcI",
   authDomain: "cata01-10ba0.firebaseapp.com",
-  projectId: "Cata01",
-  storageBucket: "cata01-10ba0.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "YOUR_APP_ID"
+  projectId: "cata01-10ba0",
+  storageBucket: "cata01-10ba0.firebasestorage.app",
+  messagingSenderId: "1030443047816",
+  appId: "1:1030443047816:web:76eb4552f630a50da6f83d",
+  measurementId: "G-WFK11V8NT1"
 };
 
 const app = initializeApp(firebaseConfig);
