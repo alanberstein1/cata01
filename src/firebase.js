41d5dd231a3c960368f0cf1f7bd62bbd1ef0ea6e
alanberstein1 <alanberstein1@gmail.com>
2025-06-30 11:09:35 -0400

    updates storage path

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAVFMND9n0LPPgKir7JK_5Sr4ANl-oiLcI",
   authDomain: "cata01-10ba0.firebaseapp.com",
   projectId: "cata01-10ba0",
-  storageBucket: "cata01-10ba0.appspot.com",
+  storageBucket: "cata01-10ba0.firebasestorage.app"
   messagingSenderId: "1030443047816",
   appId: "1:1030443047816:web:76eb4552f630a50da6f83d",
   measurementId: "G-WFK11V8NT1"
